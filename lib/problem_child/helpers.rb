8c39b45710b4ccdc7f0219878f7d3ee35ba7b4e8
Ben Balter <ben.balter@github.com>
2015-06-29 15:23:59 -0400

    add support for file uplaods

--- lib/problem_child/helpers.rb
+++ lib/problem_child/helpers.rb
@@ -26,7 +26,7 @@ module ProblemChild
     end
 
     def issue_body
-      form_data.reject { |key, value| key == "title" || value.empty? || key == "labels" }.map { |key,value| "* **#{key.humanize}**: #{value}"}.join("\n")
+      form_data.reject { |key, value| key == "title" || value.empty? || key == "labels" || value.is_a?(Hash) }.map { |key,value| "* **#{key.humanize}**: #{value}"}.join("\n")
     end
 
     # abstraction to allow cached form data to be used in place of default params
@@ -38,9 +38,69 @@ module ProblemChild
       form_data["labels"].join(",") if form_data["labels"]
     end
 
+    def uploads
+      form_data.select do |key, value|
+        value.is_a?(Hash) && ( value.has_key?("filename") || value.has_key?(:filename) )
+      end
+    end
+
     def create_issue
       issue = client.create_issue(repo, form_data["title"], issue_body, :labels => labels)
       issue["number"] if issue
+    end
+
+    # Returns array of Octokit branch objects
+    def branches
+      client.branches(repo)
+    end
+
+    # Head SHA of default branch, used for creating new branches
+    def base_sha
+      default_branch = client.repo(repo)[:default_branch]
+      branches.find { |branch| branch[:name] == default_branch }[:commit][:sha]
+    end
+
+    def branch_exists?(branch)
+      branches.any? { |b| b.name == branch }
+    end
+
+    # Name of branch to submit pull request from
+    # Starts with patch-1 and keeps going until it finds one not taken
+    def patch_branch
+      num = 1
+      branch = form_data["title"].parameterize
+      return branch unless branch_exists?(branch)
+      branch = "patch-#{num}"
+      while branch_exists?(branch) do
+        num = num + 1
+        branch = "patch-#{num}"
+      end
+      branch
+    end
+
+    # Create a branch with the given name, based off of HEAD of the defautl branch
+    def create_branch(branch)
+      client.create_ref repo, "heads/#{branch}", base_sha
+    end
+
+    # Create a pull request with the form contents
+    def create_pull_request
+      unless uploads.empty?
+        branch = patch_branch
+        create_branch(branch)
+        uploads.each do |key, upload|
+          client.create_contents(
+            repo,
+            upload["filename"],
+            "Create #{upload["filename"]}",
+            session["file_#{key}"],
+            :branch => branch
+          )
+          session["file_#{key}"] = nil
+        end
+      end
+      pr = client.create_pull_request(repo, "master", branch, form_data["title"], issue_body, :labels => labels)
+      pr["number"] if pr
     end
 
     def repo_access?
@@ -48,6 +108,13 @@ module ProblemChild
       !client.repository(repo)["private"]
     rescue
       false
+    end
+
+    def cache_form_data
+      uploads.each do |key, upload|
+        session["file_#{key}"] = File.open(upload[:tempfile]).read
+      end
+      session[:form_data] = params.to_json
     end
 
     def auth!
