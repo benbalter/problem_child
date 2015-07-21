cb0c9086425fdebcf97a2e4f400e0dc2e4c502ef
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-21 17:19:46 -0400

    #11 - recaptcha support (requires JavaScript)

--- lib/problem_child/helpers.rb
+++ lib/problem_child/helpers.rb
@@ -17,6 +17,10 @@ module ProblemChild
       end
     end
 
+    def recaptcha
+      ENV["RECAPTCHA_SITE_KEY"] unless ENV["RECAPTCHA_SITE_KEY"].nil? || ENV["RECAPTCHA_SITE_KEY"].to_s.empty?
+    end
+
     def client
       @client ||= Octokit::Client.new :access_token => token
     end
